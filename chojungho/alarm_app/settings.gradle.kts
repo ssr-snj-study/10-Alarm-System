bf5a41aca23f9af07d4fcfaea88916586ba2dc4d
jhcho <cho418651@ssrinc.co.kr>
2024-12-05 10:13:01 +0900

    .

--- chojungho/alarm_app/settings.gradle.kts
+++ chojungho/alarm_app/settings.gradle.kts
@@ -21,4 +21,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "alarm_app"
 include(":app")
- 
+
--- chojungho/alarm_service/src/api/v1/alarm/services.py
+++ chojungho/alarm_service/src/api/v1/alarm/services.py
@@ -15,13 +15,13 @@ class AlarmServices:
         self.rabbitmq = rabbitmq
 
     async def get_one(
-            self,
-            request_alarm: RequestAlarm,
-            ) -> list[AlarmDomain] | None:
+        self,
+        request_alarm: RequestAlarm,
+    ) -> list[AlarmDomain] | None:
         _alarm_domain: AlarmDomain = AlarmDomain(
             user_id=request_alarm.to["user_id"],
             email=request_alarm.from_["email"],
-            )
+        )
         alarm_domains: list[AlarmDomain] | None = await self.alarm_repository.one(_alarm_domain)
         if alarm_domains is None:
             return None
@@ -31,9 +31,14 @@ class AlarmServices:
     async def insert_queue(self, message: str, alarm_domain: AlarmDomain) -> None:
         # 큐에 넣는 작업
         alarm_domain_to_dict: dict = alarm_domain.to_dict()
-        alarm_domain_to_dict.update({"message": message, "created_at": str(alarm_domain_to_dict['created_at']),
-                                     "last_logged_in_at": str(alarm_domain_to_dict['last_logged_in_at']),
-                                     "phone_number":str(alarm_domain_to_dict['phone_number'])})
+        alarm_domain_to_dict.update(
+            {
+                "message": message,
+                "created_at": str(alarm_domain_to_dict["created_at"]),
+                "last_logged_in_at": str(alarm_domain_to_dict["last_logged_in_at"]),
+                "phone_number": str(alarm_domain_to_dict["phone_number"]),
+            }
+        )
         queue_body = json.dumps(alarm_domain_to_dict).encode()
         channel = self.rabbitmq.channel()
         device_type = {1: "Android", 2: "ios", 3: "sms", 4: "email"}
